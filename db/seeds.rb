d71f9487c8f8227a06075194429434ecbee23f59
EC2 Default User <ec2-user@ip-172-31-93-230.ec2.internal>
2023-02-23 04:14:29 +0000

    connected to Heroku, added movies to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,11 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'Finding Nemo', :rating => 'G',
+    :release_date => '30-May-2003'},
+  {:title => 'Toy Story', :rating => 'G',
+    :release_date => '02-Nov-1995'}
 ]
 
 more_movies.each do |movie|
